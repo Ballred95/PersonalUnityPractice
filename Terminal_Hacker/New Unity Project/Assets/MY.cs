89c0e36ee8f2e559540648dcf90fb9c634da155c
Blake <blake.allred95@icloud.com>
2020-01-15 16:00:56 -0700

    Added script

---
+++ Terminal_Hacker/New Unity Project/Assets/MY.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MY : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu ()
+    {
+        print("Hello Console, how are ya");
+        string greeting = "Hello there";
+        Terminal.WriteLine(greeting);
+        
+        Terminal.WriteLine("What would you like to do?");
+        Terminal.WriteLine("This");
+        Terminal.WriteLine("OR this");
+        Terminal.WriteLine("OORRRR THIS?");
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
